6c9a55cba13895d87db5f4ee6dcd674f127d02c4
mdupree <mdupree@acromedia.com>
2022-12-02 19:21:26 -0800

    Day 2.1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,8 +10,9 @@ const INPUT_SRC: &str = "input.txt";
 fn main() {
     println!("Day 2 :: Rock Paper Sissors");
     let hands: HashMap<char, &str> = HashMap::from(
-        [('A', "ROCK"), ('B', "PAPER"), ('C', "SCISSORS"), ('X', "ROCK"), ('Y', "PAPER"), ('Z', "SCISSORS")]);       
-    let hand_rankings: HashMap<&str, &str> = HashMap::from([("ROCK", "SCISSORS"), ("PAPER", "ROCK"), ("SCISSORS", "PAPER")]);       
+        [('A', "ROCK"), ('B', "PAPER"), ('C', "SCISSORS"), ('X', "LOSE"), ('Y', "DRAW"), ('Z', "WIN")]);       
+    let losing_hand: HashMap<&str, &str> = HashMap::from([("ROCK", "SCISSORS"), ("PAPER", "ROCK"), ("SCISSORS", "PAPER")]);       
+    let winning_hand: HashMap<&str, &str> = HashMap::from([("SCISSORS", "ROCK"), ("ROCK", "PAPER"), ("PAPER", "SCISSORS")]);       
     let points_per_hand: HashMap<&str, i32> = HashMap::from([("ROCK", 1), ("PAPER", 2), ("SCISSORS", 3)]);
      
     let contents = fs::read_to_string(INPUT_SRC)
@@ -21,15 +22,13 @@ fn main() {
 
     for line in contents.lines() {
         let opponent_hand = hands.get(&line.chars().nth(0).unwrap()).unwrap();
-        let player_hand = hands.get(&line.chars().nth(2).unwrap()).unwrap();
-        //println!("index : {}", opponent_hands.get(&line.chars().nth(0).unwrap());
-        println!("{} VS {}", opponent_hand, player_hand);
-        if hand_rankings.get(player_hand).unwrap() == opponent_hand {
-            player_points += 6 + points_per_hand.get(player_hand).unwrap();
-        } else if player_hand == opponent_hand {
-            player_points += 3 + points_per_hand.get(player_hand).unwrap();
-        } else {
-            player_points += 0 + points_per_hand.get(player_hand).unwrap();
+        let player_move = hands.get(&line.chars().nth(2).unwrap()).unwrap();
+        
+        println!("{} VS {}", opponent_hand, player_move);
+        match player_move {
+           &"WIN" => player_points += 6 + points_per_hand.get(winning_hand.get(opponent_hand).unwrap()).unwrap(),
+           &"DRAW" => player_points += 3 + points_per_hand.get(opponent_hand).unwrap(),
+           _ => player_points += 0 + points_per_hand.get(losing_hand.get(opponent_hand).unwrap()).unwrap(),
         }
     }
 
