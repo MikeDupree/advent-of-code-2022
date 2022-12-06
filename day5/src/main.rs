57deee34ca6d56c9c26e3cbeb7b21e9bf6aa61e2
mdupree <mdupree@acromedia.com>
2022-12-05 21:27:28 -0800

    Day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,40 +5,38 @@ fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
     let skip = [" ", "[", "]"];
-    let mut stack1: Vec<char> = Vec::new();
-    let mut stack2: Vec<char> = Vec::new();
-    let mut stack3: Vec<char> = Vec::new();
-    let mut stack4: Vec<char> = Vec::new();
-    let mut stack5: Vec<char> = Vec::new();
-    let mut stack6: Vec<char> = Vec::new();
-    let mut stack7: Vec<char> = Vec::new();
-    let mut stack8: Vec<char> = Vec::new();
-    let mut stack9: Vec<char> = Vec::new();
+    let mut stack = vec![
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+    ];
 
     let mut stack_index = 0;
     let mut line_count = 0;
     let mut stacks_complete = false;
     for line in contents.lines() {
-
         if line_count == 8 {
             stacks_complete = true;
         }
         if !stacks_complete {
             for c in line.chars() {
                 if !skip.contains(&c.to_string().as_str()) {
-                    if stack_index == 2 {
-                        stack1.push(c);
-                    }
                     match stack_index {
-                        1 => stack1.push(c),
-                        5 => stack2.push(c),
-                        9 => stack3.push(c),
-                        13 => stack4.push(c),
-                        17 => stack5.push(c),
-                        21 => stack6.push(c),
-                        25 => stack7.push(c),
-                        29 => stack8.push(c),
-                        33 => stack9.push(c),
+                        1 => stack[0].push(c),
+                        5 => stack[1].push(c),
+                        9 => stack[2].push(c),
+                        13 => stack[3].push(c),
+                        17 => stack[4].push(c),
+                        21 => stack[5].push(c),
+                        25 => stack[6].push(c),
+                        29 => stack[7].push(c),
+                        33 => stack[8].push(c),
                         _ => (),
                     }
                 }
@@ -48,36 +46,40 @@ fn main() {
 
         stack_index = 0;
         if line_count == 9 {
-            stack1.reverse();
-            stack2.reverse();
-            stack3.reverse();
-            stack4.reverse();
-            stack5.reverse();
-            stack6.reverse();
-            stack7.reverse();
-            stack8.reverse();
-            stack9.reverse();
+            for n in 0..9 {
+                stack[n].reverse();
+                println!("stack {} {:?}", n, stack[n]);
+            }
         }
 
         if line_count > 9 {
             // read the instruction
-            let inst: String = line.chars().filter(|c| c.is_digit(10)||c.is_whitespace()).collect();
-            let numbers: Result<Vec<i32>, _> = inst.split_whitespace().map(str::parse).collect();
-
+            let inst: String = line
+                .chars()
+                .filter(|c| c.is_digit(10) || c.is_whitespace())
+                .collect();
+            let result: Result<Vec<i32>, _> = inst.split_whitespace().map(str::parse).collect();
+            let instructions = result.ok().unwrap();
             // [amount to move, from, to]
-            println!("{:?}", numbers.ok().unwrap());
+            println!("{:?}", instructions);
+            let amount: i32 = instructions[0];
+            let from: i32 = instructions[1] - 1;
+            let to: i32 = instructions[2] - 1;
+            for _i in 0..amount {
+                //println!("from :: {:?}", stack[from as usize]);
+                //println!("to :: {:?}", stack[to as usize]);
+                let item = stack[from as usize].pop();
+                //println!("{}", item.unwrap());
+
+                stack[to as usize].push(item.unwrap());
+                //println!("from :: {:?}", stack[from as usize]);
+                //println!("to :: {:?}", stack[to as usize]);
+            }
         }
 
         line_count += 1;
     }
-
-    println!("Total: {:?}", stack1);
-    println!("Total: {:?}", stack2);
-    println!("Total: {:?}", stack3);
-    println!("Total: {:?}", stack4);
-    println!("Total: {:?}", stack5);
-    println!("Total: {:?}", stack6);
-    println!("Total: {:?}", stack7);
-    println!("Total: {:?}", stack8);
-    println!("Total: {:?}", stack9);
+    for n in 0..9 {
+        println!("stack {} {:?}", n, stack[n]);
+    }
 }
