01c687535b65e49527473cf2226573a921198c41
mdupree <mdupree@acromedia.com>
2022-12-11 09:29:36 -0800

    day9.1 wip

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,22 +7,19 @@ fn main() {
 }
 
 fn follow_head(c: &str) {
-    let moves = HashMap::from([
-        ('U', ["y", "1"]),
-        ('D', ["y", "-1"]),
-        ('R', ["x", "1"]),
-        ('L', ["x", "-1"]),
-    ]);
-    let mut tail_moves = 0;
-    let mut tpos_x = "0";
-    let mut tpos_y = "0";
-    let mut tgrid
-    let mut positions: Vec<String> = vec![String::from("0,0")];
+    let x = 0;
+    let y = 1;
+    let axis = HashMap::from([('U', 1), ('D', -1), ('L', -1), ('R', 1)]);
+    let coord = HashMap::from([('U', y), ('D', y), ('L', x), ('R', x)]);
+    let reset_coord = HashMap::from([('U', x), ('D', x), ('L', y), ('R', y)]);
+    let mut tail_positions = vec![[0, 0]];
+    let mut head_position = [0, 0];
+
     for line in c.lines() {
-        //  print!("{} ", line);
         let mut chars = line.chars().collect::<Vec<char>>();
-        let m = chars[0];
+        let dir = chars[0];
         chars.drain(0..2);
+
         let n: u32 = chars
             .into_iter()
             .collect::<String>()
@@ -30,191 +27,68 @@ fn follow_head(c: &str) {
             .parse::<u32>()
             .ok()
             .unwrap();
-        let mc = moves.get(&m).unwrap();
 
-        //print!("m {} ", m);
-        //print!("n {} \n", n);
-        //println!("move coords {:?}", mc);
+        // Move Head / Tail one position at a time
+        for i in 0..n {
+            // Update head position
+            head_position[*coord.get(&dir).unwrap()] += axis.get(&dir).unwrap();
 
-        // moved one space check tail pos to see if it moves
-        if n == 1 {
-            // they are overlapping
-            if tpos_x == "0" && tpos_y == "0" {
-                // move tail pos
-                if mc[0] == "y" {
-                    if mc[1] == "-1" {
-                        tpos_y = "1"
-                    } else {
-                        tpos_y = "-1"
+            // Check for tail movement
+            // xi :: -1
+            // yi :: -1
+            // yi :: 0
+            // yi :: 1
+            // xi :: 0
+            // yi :: -1
+            // yi :: 0
+            // yi :: 1
+            // xi :: 1
+            // yi :: -1
+            let mut head_in_range = false;
+            for xi in -1..2 {
+                //println!(" xi :: {}", xi);
+                for yi in -1..2 {
+                    //println!(" yi :: {}", yi);
+                    if tail_positions.last().unwrap()[x] + xi == head_position[x]
+                        && tail_positions.last().unwrap()[y] + yi == head_position[y]
+                    {
+                        head_in_range = true;
                     }
                 }
-                if mc[0] == "x" {
-                    if mc[1] == "-1" {
-                        tpos_x = "1"
-                    } else {
-                        tpos_x = "-1"
-                    }
-                }
-                positions.push(format!("{},{}", tpos_x, tpos_y));
+            }
 
+            // Head is in range. No tail movement.
+            if head_in_range {
                 continue;
             }
 
-            // handle when movement causes overlap
-            // single movement of head that overlaps tail
-            // does not trigger movement
-            if mc[0] == "y" && mc[1] == tpos_y && tpos_x == "0"
-                || mc[0] == "x" && mc[1] == tpos_x && tpos_y == "0"
-            {
-                tpos_y = "0";
-                tpos_x = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            }
-
-            // tail is adjacent and move causes tail pos to become diagonal
-            if mc[0] == "y" && tpos_y == "0" && tpos_x != "0" {
-                if mc[1] == "1" {
-                    tpos_y = "-1";
-                } else {
-                    tpos_y = "1";
-                }
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-                continue;
-            }
-
-            if mc[0] == "x" && tpos_x == "0" && tpos_y != "0" {
-                if mc[1] == "1" {
-                    tpos_x = "-1";
-                } else {
-                    tpos_x = "1";
-                }
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-                continue;
-            }
-
-            // tail is diagonal but there is no tail movement
-            // update the tail pos for one coord
-            if mc[0] == "y" && mc[1] == tpos_y && tpos_x != "0" {
-                tpos_y = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            } else if mc[0] == "x" && mc[1] == tpos_x && tpos_y != "0" {
-                tpos_x = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            }
-            // single movement that moves the tail
-            if mc[0] == "y" && mc[1] == "1" && tpos_y == "-1" {
-                tail_moves += 1;
-                tpos_x = "0"; // x is centered because of move
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            } else if mc[0] == "y" && mc[1] == "-1" && tpos_y == "1" {
-                tpos_y = "1";
-                tpos_x = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            }
-
-            if mc[0] == "x" && mc[1] == "1" && tpos_x == "-1" {
-                tail_moves += 1;
-                tpos_y = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            } else if mc[0] == "x" && mc[1] == "-1" {
-                tail_moves += 1;
-                tpos_y = "0";
-                positions.push(format!("{},{}", tpos_x, tpos_y));
-
-                continue;
-            }
-
-            println!("");
-            println!("");
-            println!("Error: single move with no case logic");
-            println!("tail ({}, {})  move {:?}", tpos_x, tpos_y, mc);
-            println!("");
-            println!("");
-            continue;
-        }
-
-        // movement is greater then one space
-        let tmb = tail_moves;
-        let mut move_mod = 0;
-        println!("\n --- \n");
-        if mc[0] == "y" {
-            // check tail loc relative to move
-            if tpos_y == "0" {
-                move_mod = 1;
-                tail_moves += n - move_mod;
-            } else if tpos_y == mc[1] {
-                move_mod = 2;
-                tail_moves += n - move_mod;
+            let mut xx = 0;
+            let mut yy = 0;
+            let mut debug = true;
+            if dir == 'U' || dir == 'D' {
+                xx = head_position[x];
+                yy = tail_positions.last().unwrap()[y] + axis.get(&dir).unwrap();
+                debug = false;
             } else {
-                move_mod = 0;
-                tail_moves += n - move_mod;
+                xx = tail_positions.last().unwrap()[x] + axis.get(&dir).unwrap();
+                yy = head_position[y]; 
+                debug = false;
             }
+            //println!("Tail before: {:?}", tail_positions.last().unwrap());
+            let new_tail_pos = [xx, yy];
 
-            if mc[1] == "-1" {
-                tpos_y = "1";
-                tpos_x = "0";
-            } else {
-                tpos_y = "-1";
-                tpos_x = "0";
+            tail_positions.push(new_tail_pos);
+            //println!("Tail After: {:?}", tail_positions.last().unwrap());
+
+            if debug {
+                println!("");
+                println!("-----------------------------");
+                println!("Error: expected to have updated tail position \n {:?}", tail_positions.last());
+                println!("-----------------------------");
+                println!("");
             }
         }
-        if mc[0] == "x" {
-            println!("handle x move");
-            // check tail loc relative to move
-            if tpos_x == "0" {
-                move_mod = 1;
-                tail_moves += n - move_mod;
-                print!(" x tpos == 0 ");
-            } else if tpos_x == mc[1] {
-                move_mod = 2;
-                tail_moves += n - move_mod;
-                print!(" x tpos == move dir ");
-            } else {
-                print!(" x else ");
-                move_mod = 0;
-                tail_moves += n - move_mod;
-            }
-
-            if mc[1] == "-1" {
-                tpos_x = "1";
-                tpos_y = "0";
-            } else {
-                tpos_x = "-1";
-                tpos_y = "0";
-            }
-        }
-
-        positions.push(format!("{},{}", tpos_x, tpos_y));
-        println!("tail moves before {} after {}", tmb, tail_moves);
-        if tail_moves != tmb {
-            // use this to debug,
-            // if the print lines at the end are hit
-            // then there is a problem
-            // TODO this check doesnt work cause some moves might not move tail...
-            continue;
-        }
-
-        println!("");
-        println!("");
-        println!("Error: multi move with no case logic");
-        println!("tail ({}, {})  move {:?}", tpos_x, tpos_y, mc);
-        println!("");
-        println!("");
-        //println!("");
     }
+    println!("positions: {:?}", tail_positions.iter().unique());
 
-    println!("positions: {}", positions.len());
-    println!("Tail Moves: {} times", tail_moves);
 }
